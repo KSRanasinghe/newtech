6ccdc3c091b3669b24bd159a0f3519a8ce903ab4
Keshara Ranasinghe <kesharashakthi71@gmail.com>
2023-06-22 09:40:35 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function change(){
+    var reg = document.getElementById("reg");
+    var log = document.getElementById("log");
+
+    reg.classList.toggle("d-none");
+    log.classList.toggle("d-none");
+}
